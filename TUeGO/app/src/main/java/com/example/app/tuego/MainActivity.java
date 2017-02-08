6c9935a439d4654462d1bec388ff1b4a3751475b
MasterForcer <lloydlo-wong@live.nl>
2017-02-08 09:50:25 +0100

    Android app initialization
    Made a start for the android app.

---
+++ TUeGO/app/src/main/java/com/example/app/tuego/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.app.tuego;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.app.tuego.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
