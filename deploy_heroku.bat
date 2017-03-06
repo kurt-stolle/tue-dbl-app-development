6d1904f3d2ebb560a16c03c4ce0424fed1160f26
Kurt Stolle <kurt@casualbananas.com>
2017-03-06 12:10:49 +0100

    Update deploy_heroku.bat

--- deploy_heroku.bat
+++ deploy_heroku.bat
@@ -1,5 +1,5 @@
 @echo off
 
-echo "This deployment script requires Heroku CLI and Git to be installed"
-echo "Pushing to Heroku master branch..."
+echo This deployment script requires Heroku CLI and Git to be installed
+echo Pushing to Heroku master branch...
 git subtree push --prefix api-server heroku master
