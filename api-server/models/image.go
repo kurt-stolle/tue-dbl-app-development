d3e6e29f6b88385afa5fcc8f7142006b011a161c
Kurt Stolle <kurt@casualbananas.com>
2017-03-28 11:31:55 +0200

    Added null value for Finder field in Images

--- api-server/models/image.go
+++ api-server/models/image.go
@@ -5,7 +5,7 @@ type Image struct {
 	UUID       string  `dbmdl:"uuid, primary key"`
 	UploadTime string  `dbmdl:"char(16), not null, default '1958-01-01 00:00'"` // Format: 2017-02-19 12:47
 	Uploader   string  `dbmdl:"uuid, not null"`
-	Finder     string  `dbmdl:"uuid not null, default ''"`
+	Finder     string  `dbmdl:"varchar(255) not null, default ''"`
 	Latitude   float64 `dbmdl:"double precision not null, default 0.0"`
 	Longitude  float64 `dbmdl:"double precision not null, default 0.0"`
 }
