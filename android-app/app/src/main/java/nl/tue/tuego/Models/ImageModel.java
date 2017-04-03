5aca8f5efc2975e9f89248df37e2bb61f61a672f
Kurt Stolle <kurt@casualbananas.com>
2017-04-03 15:50:04 +0200

    Static token store in WebAPI class

--- android-app/app/src/main/java/nl/tue/tuego/Models/ImageModel.java
+++ android-app/app/src/main/java/nl/tue/tuego/Models/ImageModel.java
@@ -2,7 +2,7 @@ package nl.tue.tuego.Models;
 
 public class ImageModel {
     public String UUID;
-    public String Uploader; // UUID of the Uploader
+    public String Uploader;     // UUID of the Uploader
     public String UploadTime; // Format: 2017-02-20 16:23
     public String Finder; // UUID of the Finder
 }
